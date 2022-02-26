5b5b382c6d3b5b40cf9b129ccc5152cf0d831193
waleed <68596366+waleed253@users.noreply.github.com>
2022-02-26 20:23:24 +0500

    Update keyboardandmouseInput.java

--- keyboardandmouseInput.java
+++ keyboardandmouseInput.java
@@ -21,13 +21,12 @@ public class keyboardandmouseInput {
 	public WebDriver driver;
 	SoftAssert softassert = new SoftAssert();
 
-	@BeforeTest
 	public void drivers() {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 	}
 	
-	@Test(priority = 1)
+	
 	public void keypress() throws Exception {
 		
 		driver.get("https://formy-project.herokuapp.com/keypress");
@@ -35,17 +34,17 @@ public class keyboardandmouseInput {
 		
 		WebElement name = driver.findElement(By.id("name"));
 		name.click();
-		Thread.sleep(2000);
+		
 		name.sendKeys("cheese");
-		Thread.sleep(2000);
+	
 		WebElement click = driver.findElement(By.id("button"));
 		click.click();
-		Thread.sleep(2000);
+		
 		driver.quit();
 		
 	}
 	
-	@Test(priority =1)
+
 	public void scroll() throws Exception {
 		driver.get("https://formy-project.herokuapp.com/scroll");
 		Thread.sleep(2000);
@@ -56,29 +55,29 @@ public class keyboardandmouseInput {
 		actions.moveToElement(fullname);
 		Thread.sleep(2000);
 		fullname.click();
-		Thread.sleep(2000);
+		
 		fullname.sendKeys("name");
-		Thread.sleep(2000);;
+		
 		date.click();
-		Thread.sleep(2000);
+		
 		date.sendKeys("28/09/2021");
-		Thread.sleep(2000);	
+			
 	}
 	
-	@Test(priority =1)
+
 	public void datepicker() throws Exception {
 		driver.get("https://formy-project.herokuapp.com/datepicker");
 		Thread.sleep(2000);
 		driver.manage().window().maximize();
 		WebElement datepicker = driver.findElement(By.id("datepicker"));
-		Thread.sleep(2000);
+		
 		datepicker.sendKeys("03/01/2022");
-		Thread.sleep(2000);
+		
 		datepicker.sendKeys(Keys.RETURN);
-		Thread.sleep(2000);
+		
 	}
 	
-	@Test(priority =1)
+
 	public void dragdrop() throws Exception {
 		driver.get("https://formy-project.herokuapp.com/dragdrop");
 		Thread.sleep(2000);
@@ -90,7 +89,7 @@ public class keyboardandmouseInput {
 		
 	}
 	
-	@Test(priority =1)
+
 	public void fileupload() throws Exception {
 		driver.get("https://formy-project.herokuapp.com/fileupload");
 		Thread.sleep(2000);
@@ -102,46 +101,40 @@ public class keyboardandmouseInput {
 	}
 	
 	
-	@Test(priority =1)
+
 	public void switchwindow()  throws Exception {
 		driver.get("https://formy-project.herokuapp.com/switch-window");
 		Thread.sleep(2000);
 		driver.manage().window().maximize();
 		WebElement btn = driver.findElement(By.id("new-tab-button"));
 		btn.click();
-		Thread.sleep(2000);
 		String originalwindow = driver.getWindowHandle();
-		Thread.sleep(2000);
 		for(String handle:driver.getWindowHandles()) {
 			driver.switchTo().window(handle);
-			//driver.switchTo().window(originalwindow);
+			driver.switchTo().window(originalwindow);
 		}
 		
 	}
 	
 	
-	@Test(priority =1)
 	public void alert() throws Exception {
 		driver.get("https://formy-project.herokuapp.com/switch-window");
 		Thread.sleep(2000);
 		driver.manage().window().maximize();
 		WebElement alert = driver.findElement(By.id("alert-button"));
 		alert.click();
-		Thread.sleep(2000);
 		
 		Alert qlert = driver.switchTo().alert();
 		qlert.accept();
 		
 	}
 	
-	@Test(priority =1)
+	
 	public void modal() throws Exception {
 		driver.get("https://formy-project.herokuapp.com/modal");
-		Thread.sleep(2000);
 		driver.manage().window().maximize();
 		WebElement modal = driver.findElement(By.id("modal-button"));
 		modal.click();
-		//Thread.sleep(2000);
 		WebElement closebtn = driver.findElement(By.id("close-button"));
 		JavascriptExecutor js = (JavascriptExecutor)driver;
 		js.executeScript("arguments[0].click();", closebtn);
